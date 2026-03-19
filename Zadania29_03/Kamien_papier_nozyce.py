ac710fddc35ee3acb37b052e77a1dbaf05eb35f8
MariaZimmer <iie.zimmer@gmail.com>
2026-03-19 19:07:33 +0100

    zrobionyP_K_N

--- Zadania29_03/Kamien_papier_nozyce.py
+++ Zadania29_03/Kamien_papier_nozyce.py
@@ -1,32 +1,28 @@
 import random
 
+wynik_uzytkownika = 0
+wynik_komputera = 0
 while True:
     ruch_uzytkownika = input("Wykonaj ruch: P/K/N/stop")
     if ruch_uzytkownika == "stop":
         print("Zakończyłeś grę")
+        print(f"Wyniki końcowe: Ty {wynik_uzytkownika}   |   Komputer: {wynik_komputera}")
         break
     else:
-        if ruch_uzytkownika == "P":
-            ruch_uzytkownika = 1
-            print("Ruch uzytkownika: Papier")
-        elif ruch_uzytkownika == "K":
-            ruch_uzytkownika = 2
-            print("Ruch uzytkownika: Kamień")
-        elif ruch_uzytkownika == "N":
-            ruch_uzytkownika = 3
-            print("Ruch uzytkownika: Nożyce")
+        ruchy = {
+            "P": "Papier",
+            "K": "Kamień",
+            "N": "Nożyce"
+        }
 
-        ruch_komputera = random.randint(1,3)
-        if ruch_komputera == 1: print("Ruch komputera: Papier")
-        elif ruch_komputera == 2: print("Ruch komputera: Kamień")
-        elif ruch_komputera == 3: print("Ruch komputera: Nożyce")
-        wyniki = []
+        ruch_komputera = random.choice(["K", "P", "N"])
+        print(f"Ty: {ruchy[ruch_uzytkownika]}   |    Komputer: {ruchy[ruch_komputera]}")
 
         if ruch_uzytkownika == ruch_komputera:
             print("Remis")
-        elif ruch_uzytkownika == 1 and ruch_komputera == 2: print("Wygrał uzytkownik")
-        elif ruch_uzytkownika == 2 and ruch_komputera == 3: print("Wygrał komputer")
-        elif ruch_uzytkownika == 3 and ruch_komputera == 1: print("Wygrał uzytkownik")
-        elif ruch_uzytkownika == 1 and ruch_komputera == 3: print("Wygrał komputer")
-        elif ruch_uzytkownika == 2 and ruch_komputera == 1: print("Wygrał komputer")
-        elif ruch_uzytkownika == 3 and ruch_komputera == 2: print("Wygrał uzytkownik")
+        elif ruch_uzytkownika == "P" and ruch_komputera == "K" : wynik_uzytkownika += 1
+        elif ruch_uzytkownika == "K" and ruch_komputera == "N" : wynik_uzytkownika += 1
+        elif ruch_uzytkownika == "N" and ruch_komputera == "P": wynik_uzytkownika += 1
+        else: wynik_komputera += 1
+
+        print(f"Aktualne wyniki: Ty {wynik_uzytkownika}   |   Komputer: {wynik_komputera}")
--- Zadania_na_rozgrzewke/LoveUEP.py
+++ Zadania_na_rozgrzewke/LoveUEP.py
@@ -1,6 +1,7 @@
+NIU = "88335"
 for i in range(1, 101):
     if i == 50:
-        print(88335)
+        print(NIU)
     elif i % 3 == 0 and i % 5 == 0:
         print("LoveUEP")
     elif i % 3 == 0:
