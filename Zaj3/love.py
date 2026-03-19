7ab12db829834ef50766fe0cd19ff4fc09d4b77f
Tymek144 <tym.pom.2g@gmail.com>
2026-03-19 18:51:50 +0100

    save zaj3

---
+++ Zaj3/love.py
@@ -0,0 +1,18 @@
+NIU = "88271"
+
+for i in range(1, 101):
+    if i == 50:
+        print(NIU)
+    elif i % 5 == 0 and i % 3 == 0:
+        print("LoveUEP")
+    elif i % 3 == 0:
+        print("Love")
+    elif i % 5 == 0:
+        print("UEP")
+    else:
+        print(i)
+
+
+
+
+
