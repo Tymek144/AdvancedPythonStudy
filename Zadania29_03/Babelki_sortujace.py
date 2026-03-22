4766f986f63b1f452864ee0e85350da23f59eba6
MariaZimmer <iie.zimmer@gmail.com>
2026-03-22 21:55:50 +0100

    sortowanie zrobione

--- Zadania29_03/Babelki_sortujace.py
+++ Zadania29_03/Babelki_sortujace.py
@@ -5,3 +5,19 @@ for znak in NIU:
         lista.append(int(znak))
 print(lista)
 
+def bubble_ascending(lista):
+    for j in range(len(lista)-1):
+        for i in range(len(lista)-1-j):
+            if lista[i] < lista[i+1]:
+                lista[i], lista[i+1] = lista[i+1], lista[i]
+    return lista
+
+def bubble_descending(lista):
+    for j in range(len(lista)-1):
+        for i in range (len(lista)-1-j):
+            if lista[i] > lista[i+1]:
+                lista[i], lista[i+1] = lista[i+1], lista[i]
+    return lista
+
+print(bubble_ascending(lista))
+print(bubble_descending(lista))
