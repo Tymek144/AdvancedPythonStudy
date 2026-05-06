36cf13bda9756ea00275356d4c59d879731dafaa
MariaZimmer <iie.zimmer@gmail.com>
2026-05-06 22:32:50 +0200

    gui zrobione

--- GUI10_03/GUIswieczki.py
+++ GUI10_03/GUIswieczki.py
@@ -1,55 +1,78 @@
-from tkinter import * # biblioteka GUI
-root = Tk() # Utworzenie obiektu okna
-root.title('MyGUIApp') # tytuł okna
-canvas = Canvas(root, height=800, width=600) # Obiektu canvas zawiera wszystkie elementy GUI
-canvas.pack() # Umieszczenie canvas w oknie
+import requests
+import json
+import pandas as pd
+import mplfinance as mpl
+from tkinter import *
 
-background_image = PhotoImage(file='altum.png')
-background_label = Label(root, image=background_image) #
-background_label.place(relwidth=1, relheight=1) # Umieszczenie Label z obrazkiem w canvas (okno programu)
-# relwidth szerokość Label równa szerokości okna
-# relheight wysokość Label równa wysokości okna
+def draw_candle_chart():
+    symbol = symbolEntry.get()
+    interval = intervalEntry.get()
+    limit = limitEntry.get()
+    url = f'https://api.mexc.com/api/v3/klines?symbol={symbol}&interval={interval}&limit={limit}'
+    response = requests.get(url)
+    responseBody = response.text
+    responseBodyJson = json.loads(responseBody)
+    print(responseBodyJson)
 
-# tworzenie ramki by pozycjonować w niej elementy
+    formattedCandlesData = []
+
+    for candle in responseBodyJson:
+        x = {
+            'time': candle[0],
+            'open': float(candle[1]),
+            'close': float(candle[4]),
+            'high': float(candle[2]),
+            'low': float(candle[3])
+        }
+        formattedCandlesData.append(x)
+
+    df = pd.json_normalize(formattedCandlesData)
+    df.time = pd.to_datetime(df.time, unit='ms')
+    df = df.set_index('time')
+
+    print(df.columns)
+    print(df.head())
+
+    mpl.plot(
+        df,
+        type="candle",
+        title=f"Candle chart: {symbol}",
+        style="yahoo",
+        mav=(3, 6, 9)
+    )
+
+# GUI
+root = Tk()
+root.title('GUI-ŚWIECZKI')
+
+canvas = Canvas(root, height=500, width=700)
+canvas.pack()
+
 frame = Frame(root, bg='#ffffff')
-frame.place(relx=0.5, rely=0.6, relwidth=0.75, relheight=0.1, anchor='n') # Wsadzenie Frame w canvas
-# anchor - punkt odniesienia, 'n' to górny punkt
+frame.place(relx=0.5, rely=0.2, relwidth=0.8, relheight=0.45, anchor='n')
 
-labelOne = Label(frame) # obiekt Label dla tekstu
-labelOne.place(relx=0.05, rely=0.15, relwidth=0.18, relheight=0.3)
-labelOne.configure(text="Label One") # Nadanie obiektowi Label tekst
-entryOne = Entry(frame, font=40)
-entryOne.place(relx=0.25, rely=0.15, relwidth=0.3, relheight=0.3)
+symbolLabel = Label(frame, text="Symbol")
+symbolLabel.place(relx=0.05, rely=0.1, relwidth=0.25, relheight=0.15)
 
-labelTwo = Label(frame)
-labelTwo.place(relx=0.05, rely=0.55, relwidth=0.18, relheight=0.3) # Umieszczenie obiektu Label w obiekcie Frame
-labelTwo.configure(text="Label One")
-entryTwo = Entry(frame, font=40)
-entryTwo.place(relx=0.25, rely=0.55, relwidth=0.3, relheight=0.3)
+symbolEntry = Entry(frame, font=40)
+symbolEntry.place(relx=0.35, rely=0.1, relwidth=0.55, relheight=0.15)
+symbolEntry.insert(0, "GEVONUSDT")
 
-entryOne.insert(0, "Hello") # Wstawienie tekstu do obiektu Entry o nazwie entryOne
-entryTwo.insert(0, "Hi") # Wstawienie tekstu do obiektu Entry o nazwie entryTwo
+intervalLabel = Label(frame, text="Interval")
+intervalLabel.place(relx=0.05, rely=0.35, relwidth=0.25, relheight=0.15)
 
-def printTextEnteredInEntryOne():
-    print(entryOne.get()) # Wyświetlenie tekstu wpisanego w Entry
+intervalEntry = Entry(frame, font=40)
+intervalEntry.place(relx=0.35, rely=0.35, relwidth=0.55, relheight=0.15)
+intervalEntry.insert(0, "30m")
 
+limitLabel = Label(frame, text="Limit")
+limitLabel.place(relx=0.05, rely=0.6, relwidth=0.25, relheight=0.15)
 
-printInputButton = Button(frame, text="Print Text", command=printTextEnteredInEntryOne) # Utworzenie Button i przypisanie do niego funkcji o nazwie printTextEnteredInEntryOne
-printInputButton.place(relx=0.6, rely=0.16, relwidth=0.35, relheight=0.7) # Umieszczenie Button w Frame
+limitEntry = Entry(frame, font=40)
+limitEntry.place(relx=0.35, rely=0.6, relwidth=0.55, relheight=0.15)
+limitEntry.insert(0, "10")
 
-secondFrame = Frame(root, bg='#ffffff')
-secondFrame.place(relx=0.5, rely=0.8, relwidth=0.75, relheight=0.1, anchor='n')
+button = Button(frame, text="Pobierz dane i narysuj wykres", command=draw_candle_chart)
+button.place(relx=0.2, rely=0.82, relwidth=0.6, relheight=0.13)
 
-selectedOption = StringVar(root, 'FIRST_OPTION') # StringVar zawiera wybraną opcję z listy
-
-def printSelectedOption(): # Funkcja wyświetlq wybraną opcję z listy
-    print(selectedOption.get()) #
-
-
-optionOne = Radiobutton(secondFrame, text="GEVON USDT", variable=selectedOption, value='GEVONUSDT') # Radiobutton zawiera opcję z listy i podpina ją do obiektu StringVar o nazwie selectedOption
-optionOne.place(relx=0.1, rely=0.15, relwidth=0.3, relheight=0.3)
-optionTwo = Radiobutton(secondFrame, text="ETH USDT", variable=selectedOption, value='ETHUSDT')
-optionTwo.place(relx=0.1, rely=0.55, relwidth=0.3, relheight=0.3)
-printSelectButton = Button(secondFrame, text="Button One", command=printSelectedOption) # przypisanie do Button funkcji printSelectedOption
-printSelectButton.place(relx=0.6, rely=0.16, relwidth=0.35, relheight=0.7)
 root.mainloop()
